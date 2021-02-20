6767802196ca2246c113482a2c52001515db1763
Kyle E. Mitchell <kyle@kemitchell.com>
2021-02-20 11:07:18 -0800

    Add CLI test

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const meta = require('./package.json')
 const os = require('os')
 const path = require('path')
 const rimraf = require('rimraf')
+const spawn = require('child_process').spawn
 const tap = require('tap')
 
 const HTML = 'text/html; charset=UTF-8'
@@ -97,7 +98,36 @@ tap.test('GET /nonexistent', test => {
   })
 })
 
-// TODO: Add a test that runs the server via bin.js.
+tap.test('CLI', test => {
+  const tmpdir = path.join(os.tmpdir(), `${meta.name}-test`)
+  // Create a temporary directory for the test.
+  fs.mkdtemp(tmpdir, (_, directory) => {
+    // Write index.html in the directory.
+    const body = '<p>index</p>'
+    const index = path.join(directory, 'index.html')
+    fs.writeFile(index, body, error => {
+      test.ifError(error, 'no error writing file')
+      // Run the CLI to serve the directory.
+      const port = 8080
+      const cli = spawn('./bin.js', ['-p', port.toString(), directory])
+      // Request index.html.
+      http.request({ port })
+        .once('response', response => {
+          test.equal(response.statusCode, 200, 'Status: 200')
+          test.equal(response.headers['content-type'], HTML, 'Content-Type: HTML')
+          const chunks = []
+          response
+            .on('data', chunk => { chunks.push(chunk) })
+            .once('end', () => {
+              test.equal(Buffer.concat(chunks).toString(), body, 'Body: as written')
+              cli.kill(9)
+              rimraf(directory, () => { test.end() })
+            })
+        })
+        .end()
+    })
+  })
+})
 
 function server (callback) {
   // Create a temporary directory for the test.
